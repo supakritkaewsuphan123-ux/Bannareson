a9d5f401181e21bbdf85a3cc48a509ddeffd0fc4
KABEE <kabee@example.com>
2026-04-17 20:15:12 +0700

    Security: Explicitly allow all origins in CORS to support new frontend deployments

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,11 @@ const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Public Routes
